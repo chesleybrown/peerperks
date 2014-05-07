0a3eb8cffb0b1d48d9ccf5fe0f7d55c1ecda36d9
Chesley Brown <chezbrown@gmail.com>
2014-05-07 20:06:21 -0230

    Setup grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,138 @@
+'use strict';
+
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
+	
+	grunt.initConfig({
+		browserify: {
+			options: {
+				debug: true
+			},
+			deps: {
+				files: {
+					'build/js/compiled/deps.js': [
+						'web/components/angular/angular.min.js',
+						'web/components/angular-route/angular-route.min.js',
+						'web/components/firebase/firebase.js',
+						'web/components/angularfire/angularfire.min.js'
+					]
+				}
+			},
+			client: {
+				files: {
+					'build/js/compiled/client.js': [
+						'web/js/client.js',
+						'web/js/directives/**/*.js',
+						'web/js/services/**/*.js',
+						'web/js/filters/**/*.js'
+					]
+				}
+			}
+		},
+		uglify: {
+			options: {
+				mangle: false
+			},
+			deps: {
+				files: {
+					'build/js/compiled/deps.js': ['build/js/compiled/deps.js']
+				}
+			},
+			client: {
+				files: {
+					'build/js/compiled/client.js': ['build/js/compiled/client.js']
+				}
+			}
+		},
+		sass: {
+			build: {
+				options: {
+					outputStyle: 'nested'
+				},
+				files: {
+					'build/css/compiled/base.css': 'web/css/base.scss'
+				}
+			}
+		},
+		cssmin: {
+			options: {
+				keepSpecialComments: 0
+			},
+			build: {
+				files: {
+					'build/css/compiled/site.css': [
+						'web/components/bootstrap/dist/css/bootstrap.min.css',
+						'web/components/bootstrap/dist/css/bootstrap-theme.min.css',
+						'build/css/compiled/base.css'
+					]
+				}
+			}
+		},
+		clean: {
+			build: [
+				'build'
+			],
+			css: [
+				'build/css/compiled'
+			],
+			js: [
+				'build/js/compiled'
+			]
+		},
+		copy: {
+			build: {
+				expand: true,
+				cwd: 'web',
+				src: [
+					'*'
+				],
+				dest: 'build/'
+			},
+			html: {
+				expand: true,
+				cwd: 'web',
+				src: [
+					'*.html'
+				],
+				dest: 'build/'
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					port: 3002,
+					hostname: '*',
+					base: 'build/',
+					livereload: 35730
+				}
+			}
+		},
+		watch: {
+			options: {
+				livereload: 35730
+			},
+			css: {
+				files: ['web/css/*.scss'],
+				tasks: ['sass:build', 'cssmin:build']
+			},
+			js: {
+				files: ['test/**/*.spec.js', 'web/js/*.js', 'web/js/directives/**/*.js', 'web/js/services/**/*.js', 'web/js/filters/**/*.js'],
+				tasks: ['browserify:client']
+			},
+			html: {
+				files: ['web/*.html'],
+				tasks: ['copy:html']
+			}
+		}
+	});
+	
+	grunt.registerTask('default', ['build', 'connect:server', 'watch']);
+	grunt.registerTask('server', ['connect:server']);
+	
+	grunt.registerTask('build', ['clean:build', 'build:css', 'build:js', 'copy:build',]);
+	grunt.registerTask('build:js', ['clean:js', 'browserify:deps', 'browserify:client',]);
+	grunt.registerTask('build:css', ['clean:css', 'sass:build', 'compress:css']);
+	grunt.registerTask('build:dist', ['build', 'compress:js',]);
+	grunt.registerTask('compress:js', ['uglify:deps', 'uglify:client']);
+	grunt.registerTask('compress:css', ['cssmin:build']);
+}
